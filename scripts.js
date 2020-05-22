382f3ad915a3f81afc0758fdee8b5fc449025b6c
SNCollins20 <Joythe1st@aol.com>
2020-05-22 16:40:04 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let paragraph = document.createElement('p')
+let text = document.createElement( )
+
+document.addEventListner('DOMContentLoaded", function)({
+
+
+    let div = document.createElement('div');
+
+let button = document.createElement('button');
+let btnText =  document.createTextNode('Add Square');
+button.appendChild(btnText);
+document.body.appendChild(btnText);
+
+let button = document.createElement('button');
+let btnText = document.createTextNode('Add Square');
+button.appendChild(btnText);
+document.body.appendChild('button');
+
+button.addEventListener("click", function(){
+    let h1 = document.createElement('h1');
+    let h1Text = document.createTextNode(" Add Square ");
+    h1.appendChild(h1Text);
+    document.body.appendChild(h1);
+})
+
+})
+
